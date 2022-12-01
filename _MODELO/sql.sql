0f29e2a1cec43fcf1f45e6ac6a5a4c740ab12b7c
SilvaNeto29 <silva.netocataguases@gmail.com>
2022-11-30 22:19:50 -0300

    new file: composer

---
+++ _MODELO/sql.sql
@@ -0,0 +1,63 @@
+-- Arquivo de configuração e arquitetura do banco de dados
+
+
+
+-- Criacao tabela usuarios
+CREATE TABLE `users` (
+`id`  int NOT NULL AUTO_INCREMENT,
+`email`  varchar(100) NULL ,
+`password`  varchar(200) NULL ,
+`birthdate`  datetime NULL ,
+`city`  varchar(100) NULL ,
+`work`  varchar(100) NULL ,
+`cover`  varchar(100) NULL ,
+`avatar`  varchar(100) NULL ,
+`toker`  varchar(200) NULL ,
+PRIMARY KEY (`id`)
+)
+;
+
+
+
+-- Criacao de tabela de relacao entre usuarios
+CREATE TABLE `userrelations` (
+`id`  int NOT NULL AUTO_INCREMENT ,
+`user_from`  int NULL ,
+`user_to`  int NULL ,
+PRIMARY KEY (`id`)
+)
+;
+
+
+
+-- Tabela de posts
+CREATE TABLE `posts` (
+`id`  int NOT NULL AUTO_INCREMENT ,
+`type`  varchar(255) NULL ,
+`created_at`  datetime NULL ,
+`body`  text NULL ,
+PRIMARY KEY (`id`)
+)
+;
+
+
+
+-- Tabela de likes de posts
+CREATE TABLE `postlikes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_post` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+-- Tabela de comentarios de posts
+CREATE TABLE `postcomments` (
+  `id_post` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `body` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
